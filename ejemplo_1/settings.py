92d4564b5a7d6a76d394716651ff0f7d69955fa8
peligro <yo@cesarcancino.com>
2019-03-16 20:48:23 -0300

    se añade CRUD de productos al admin :facepunch:

--- ejemplo_1/settings.py
+++ ejemplo_1/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'ejemplo_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'peligro',
+        'PASSWORD': 'ZeQFHSYtdSUoXRtg:9K3',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from home.models import Productos
 
-# Register your models here.
+
+class ProductosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'titulo', 'epigrafe', 'fecha')
+    list_filter = ('id', 'url', 'titulo', 'epigrafe', 'fecha')
+    search_fields = ('id', 'url', 'titulo', 'epigrafe', 'fecha')
+
+
+admin.site.register(Productos, ProductosAdmin)
